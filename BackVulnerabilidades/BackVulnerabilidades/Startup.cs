9df73d566b84f6318dd32150e1469f4891ee2e89
Jazmin <21750959@live.uem.es>
2022-06-13 23:31:33 +0200

    Configuración inicial + swagger

--- BackVulnerabilidades/BackVulnerabilidades/Startup.cs
+++ BackVulnerabilidades/BackVulnerabilidades/Startup.cs
@@ -1,29 +1,81 @@
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.AspNetCore.Localization;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.OpenApi.Models;
+using Swashbuckle.AspNetCore.Filters;
 using System;
 using System.Collections.Generic;
+using System.Globalization;
+using System.IO;
 using System.Linq;
+using System.Reflection;
 using System.Threading.Tasks;
 
 namespace BackVulnerabilidades
 {
     public class Startup
     {
+        readonly string MyAllowSpecificOrigins = "EnableCORS";
         public Startup(IConfiguration configuration)
         {
             Configuration = configuration;
+            StaticConfig = configuration;
         }
 
+        public static IConfiguration StaticConfig { get; private set; }
         public IConfiguration Configuration { get; }
 
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllersWithViews();
+
+            // Register the Swagger generator, defining 1 or more Swagger documents
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new OpenApiInfo
+                {
+                    Version = "v1",
+                    Title = "Remesas - Docentación de la API",
+                    Description = "Documentación de la API para la aplicación de remesas.",
+                    // TermsOfService = new Uri("https://example.com/terms"),
+                    Contact = new OpenApiContact
+                    {
+                        Name = "Soft Control Informática",
+                        Email = "zparellada@soft-control.net",
+                        Url = new Uri("https://soft-control.net"),
+                    },
+                    License = new OpenApiLicense
+                    {
+                        Name = "Uso exclusivo de Soft Control Informática."
+                    }
+                });
+
+                // To Enable authorization using Swagger (JWT)  
+                c.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme()
+                {
+                    Name = "Authorization",
+                    Type = SecuritySchemeType.ApiKey,
+                    Scheme = "Bearer",
+                    BearerFormat = "JWT",
+                    In = ParameterLocation.Header,
+                    Description = "JWT Authorization header using the Bearer scheme. \r\n\r\n Enter 'Bearer' [space] and then your token in the text input below.\r\n\r\nExample: \"Bearer 12345abcdef\"",
+                });
+
+                c.EnableAnnotations();
+                c.ExampleFilters();
+
+                // Set the comments path for the Swagger JSON and UI.
+                var xmlFile = $"{Assembly.GetExecutingAssembly().GetName().Name}.xml";
+                var xmlPath = Path.Combine(AppContext.BaseDirectory, xmlFile);
+                // c.IncludeXmlComments(xmlPath, includeControllerXmlComments: true);
+            });
+            services.AddSwaggerExamples();
+            // services.AddSwaggerExamplesFromAssemblyOf(typeof(EjemploErroresLogin));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -39,18 +91,38 @@ namespace BackVulnerabilidades
                 // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
                 app.UseHsts();
             }
-            app.UseHttpsRedirection();
-            app.UseStaticFiles();
+            app.UseCors(MyAllowSpecificOrigins);
 
+            app.UseHttpsRedirection();
+
+            var cultureInfo = new CultureInfo("es-ES");
+            CultureInfo.CurrentCulture = cultureInfo;
+            CultureInfo.CurrentUICulture = cultureInfo;
+
+
+            app.UseRequestLocalization(new RequestLocalizationOptions
+            {
+                DefaultRequestCulture = new RequestCulture("es-ES"),
+                // Formatting numbers, dates, etc.
+                SupportedCultures = new[] { new CultureInfo("es-ES") },
+                // UI strings that we have localized.
+                SupportedUICultures = new[] { new CultureInfo("es-ES") }
+            });
+            app.UseStaticFiles();
             app.UseRouting();
 
+            app.UseAuthentication();
             app.UseAuthorization();
+
+            // Enable middleware to serve generated Swagger as a JSON endpoint.
+            app.UseSwagger(c => c.SerializeAsV2 = true);
+            app.UseSwaggerUI();
 
             app.UseEndpoints(endpoints =>
             {
                 endpoints.MapControllerRoute(
                     name: "default",
-                    pattern: "{controller=Home}/{action=Index}/{id?}");
+                    pattern: "{controller}/{action=Index}/{id?}");
             });
         }
     }
