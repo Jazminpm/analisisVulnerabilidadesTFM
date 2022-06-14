bc35c797767b3b1dd853f8f375e5772420b2e01d
Jazmin <21750959@live.uem.es>
2022-06-14 21:45:50 +0200

    Panel principal

--- FrontVulnerabilidades/src/app/app-routing.module.ts
+++ FrontVulnerabilidades/src/app/app-routing.module.ts
@@ -1,7 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import {ContenedorComponent} from "./@core/componentes/contenedor/contenedor.component";
+import {EscanerSistemaOperativoComponent} from "./@core/componentes/analisis/escaner-sistema-operativo/escaner-sistema-operativo.component";
+import {EscanerPuertosComponent} from "./@core/componentes/analisis/escaner-puertos/escaner-puertos.component";
+import {DescubrimientoActivosComponent} from "./@core/componentes/analisis/descubrimiento-activos/descubrimiento-activos.component";
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', component: ContenedorComponent },
+  { path: 'descubrimiento-activos', component: DescubrimientoActivosComponent },
+  { path: 'escaner-puertos', component: EscanerPuertosComponent },
+  { path: 'escaner-sistema-operativo', component: EscanerSistemaOperativoComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
